56e58a69ff17807852f356da5c9f0c47752c1fe7
Charlie Kilner <40582124+charliexkilner@users.noreply.github.com>
2021-12-18 23:09:02 +0000

    Add files via upload

--- taskpane.js
+++ taskpane.js
@@ -3,11 +3,54 @@
  * See LICENSE in the project root for license information.
  */
 
-/* global document, Office, require */
-const ssoAuthHelper = require("./../helpers/ssoauthhelper");
+/* global document, Office, Word */
 
-Office.onReady((info) => {
-  if (info.host === Office.HostType.Word) {
-    document.getElementById("getGraphDataButton").onclick = ssoAuthHelper.getGraphData;
-  }
-});
+$("#run").click(() => tryCatch(run));
+
+function run() {
+  return Word.run(function(context) {
+    var range = context.document.getSelection();
+    range.font.color = "red";
+    range.load("text");
+
+    return context.sync().then(function() {
+      console.log('The selected text was "' + range.text + '".');
+    });
+  });
+}
+
+/** Default helper for invoking an action and handling errors. */
+function tryCatch(callback) {
+  Promise.resolve()
+    .then(callback)
+    .catch(function(error) {
+      // Note: In a production add-in, you'd want to notify the user through your add-in's UI.
+      console.error(error);
+    });
+}
+
+const OpenAI = require('openai-api');
+
+// Load your key from an environment variable or secret management service
+// (do not include your key directly in your code)
+const OPENAI_API_KEY = process.env.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;
+
+const openai = new OpenAI(OPENAI_API_KEY);
+
+(async () => {
+  const gptResponse = await openai.complete({
+      engine: 'davinci',
+      prompt: 'this is a test',
+      maxTokens: 5,
+      temperature: 0.9,
+      topP: 1,
+      presencePenalty: 0,
+      frequencyPenalty: 0,
+      bestOf: 1,
+      n: 1,
+      stream: false,
+      stop: ['\n', "testing"]
+  });
+
+  console.log(gptResponse.data);
+})();
